b52098ad4f9b03cefecf5a9c6b8e2ca64b8a080d
Emma Sindberg Jul <emmasindbergjul@gmail.com>
2026-05-07 09:43:52 +0200

    Added tidy_survey_dates function

--- R/functions.R
+++ R/functions.R
@@ -81,3 +81,22 @@ summarise_by_datetime <- function(data) {
     )
   return(summarised_data)
 }
+
+#' Tidy up the dates in the survey results
+#'
+#' @param data A data frame of the survey results data.
+#'
+#' @return A data frame.
+tidy_survey_dates <- function(data) {
+  tidied <- data %>%
+    dplyr::mutate(
+      date = lubridate::mdy(date),
+      start_datetime = as_datetime(paste(date, start_time)),
+      end_datetime = as_datetime(paste(date, end_time)),
+      datetime_id = start_datetime,
+      .before = start_time
+    ) %>%
+    dplyr::select(-c(start_time, date, end_time, duration))
+
+  return(tidied)
+}
